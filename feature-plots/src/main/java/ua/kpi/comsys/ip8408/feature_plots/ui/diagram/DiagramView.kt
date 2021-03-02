da55ea9e3e7247a7397431e1b09e7d2838eb195f
NeProgramist <zaskozhenya@gmail.com>
2021-03-02 13:29:37 +0200

    feat(lab2): create separate module for student info and plots

---
+++ feature-plots/src/main/java/ua/kpi/comsys/ip8408/feature_plots/ui/diagram/DiagramView.kt
@@ -0,0 +1,16 @@
+package ua.kpi.comsys.ip8408.feature_plots.ui.diagram
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DiagramView : View {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr)
+}
